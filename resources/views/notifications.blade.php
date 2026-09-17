68415d49c85a3c1440ef5d451ac369dd7a4f9ddc
Tom Sharrock <tsharrock@gmail.com>
2026-09-17 12:50:38 -0600

    added move notification

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -9,23 +9,35 @@
     <section class="py-5">
         <div class="container">
             <h1>We're Moving</h1>
-            <p>
-                We're excited to share that Redmond Medical & Mental Health is moving to a new
-                location in Logan, Utah. Our last day at our current address is
-                <strong>October 9th</strong>. Our phone number, providers, and services remain the
-                same &mdash; only our address is changing.
-            </p>
-            <p>
-                <strong>Our new address (starting October 10th):</strong><br>
-                Redmond Medical & Mental Health<br>
-                196 South 100 West, Suite 101<br>
-                Logan, Utah 84321
-            </p>
-            <p>
-                If you have an upcoming appointment or questions about the move, please
-                <a href="{{ route('contact') }}">contact us</a> or call us at
-                <a href="tel:4359381440">(435) 938-1440</a>.
-            </p>
+            <div class="row align-items-start">
+                <div class="col-md-7">
+                    <p>
+                        We're excited to share that Redmond Medical & Mental Health is moving to a new
+                        location in Logan, Utah. Our last day at our current address is
+                        <strong>October 9th</strong>. Our phone number, providers, and services remain the
+                        same &mdash; only our address is changing.
+                    </p>
+                    <p>
+                        <strong>Our new address (starting October 10th):</strong><br>
+                        Redmond Medical & Mental Health<br>
+                        196 South 100 West, Suite 101<br>
+                        Logan, Utah 84321
+                    </p>
+                    <p>
+                        If you have an upcoming appointment or questions about the move, please
+                        <a href="{{ route('contact') }}">contact us</a> or call us at
+                        <a href="tel:4359381440">(435) 938-1440</a>.
+                    </p>
+                </div>
+                <div class="col-md-5 mt-4 mt-md-0">
+                    <div class="ratio ratio-16x9 border">
+                        <iframe title="Map to our new location at 196 South 100 West, Suite 101, Logan, UT 84321"
+                            src="https://www.google.com/maps?q=196+South+100+West+Suite+101+Logan+UT+84321&output=embed"
+                            style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
